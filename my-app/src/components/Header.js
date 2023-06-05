ad98e48b68371a73607b1d2b83923f03e2fdee90
Gina <gmroscioli@gmail.com>
2023-06-05 15:49:57 -0400

    add header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,16 @@
-import App from "./App";
+import React from "react";
 
-function Header(){
-    return(
-        <div>
-
-        </div>
-    )
+function Header() {
+  return (
+    <header>
+      <h1>
+        Harry Potter Movies
+        <span className="logo" role="img">
+          🪄
+        </span>
+      </h1>
+    </header>
+  );
 }
+
 export default Header;

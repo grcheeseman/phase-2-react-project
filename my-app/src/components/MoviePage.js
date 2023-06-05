4ea436f56d6f1257015b798a0dec83473d2e5131
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-06-05 13:14:24 -0600

    basic Component structure added

---
+++ my-app/src/components/MoviePage.js
@@ -0,0 +1,15 @@
+import App from "./App";
+import MovieCollection from "./MovieCollection";
+
+function MoviePage(){
+    return (
+        <div>
+        <MovieCollection/>
+
+        </div>
+
+    )
+   
+}
+
+export default MoviePage;
